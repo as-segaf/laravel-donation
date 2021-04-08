680990a582b604568638ebf78e77b6ba0ec6f940
as-segaf <ziyanassegaf20@gmail.com>
2021-04-09 01:55:47 +0700

    get snap token

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Interfaces\OrderRepositoryInterface;
+use Midtrans;
 
 class OrderService
 {
@@ -13,8 +14,32 @@ class OrderService
         return $this->orderRepository = $orderRepository;
     }
 
+    public function getSnapTokenMidtrans($order_id, $gross_amount)
+    {
+        Midtrans\Config::$serverKey = 'SB-Mid-server-N8nj5ENZHnSH7nvZ14nuCH94';
+        Midtrans\Config::$isProduction = false;
+        Midtrans\Config::$isSanitized = true;
+        Midtrans\Config::$is3ds = true;
+
+        $snapToken = Midtrans\Snap::getSnapToken([
+            'transaction_details' => [
+                'order_id' => $order_id,
+                'gross_amount' => $gross_amount
+            ]
+        ]);
+
+        if (!$snapToken) {
+            throw new Exception("Failed to get snap token midtrans", 1);
+        }
+
+        return $snapToken;
+    }
+
     public function store($request)
     {
-        return $this->orderRepository->createOrder($request);
+        $order = $this->orderRepository->createOrder($request);
+        $order['snapToken'] = $this->getSnapTokenMidtrans($order->id, $order->amount);
+
+        return $order;
     }
 }
